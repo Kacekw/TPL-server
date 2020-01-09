420ac1b4bb7e7c58d604b23c0f814525bb01589e
Kacper Witos <kaclaw@gmail.com>
2020-01-09 10:18:40 +0100

    PowerPointLayer update with presentation run/close

--- src/main/java/com/vestas/kawit/moms/service/power_point/PowerPointLayer.java
+++ src/main/java/com/vestas/kawit/moms/service/power_point/PowerPointLayer.java
@@ -1,37 +1,43 @@
 package com.vestas.kawit.moms.service.power_point;
 
 import com.jacob.activeX.ActiveXComponent;
+import com.jacob.com.ComFailException;
 import com.jacob.com.Dispatch;
-import com.jacob.com.Variant;
 
 public class PowerPointLayer {
 
     private Dispatch presentation;
     private Dispatch presentations;
+    private Dispatch slideShowSettings;
     private ActiveXComponent powerPoint;
 
 
     public Result runPresentation(String filePath) {
+
         System.out.println(stopPresentation());
 
-        powerPoint = new ActiveXComponent("PowerPoint.Application");
-        presentations = powerPoint.getProperty("Presentations").toDispatch();
+        if (powerPoint == null) powerPoint = new ActiveXComponent("PowerPoint.Application");
+        if (presentations == null) presentations = powerPoint.getProperty("Presentations").toDispatch();
         presentation = Dispatch.call(presentations, "Open", filePath).toDispatch();
+        slideShowSettings = Dispatch.call(presentation, "SlideShowSettings").toDispatch();
+
+        Dispatch.call(slideShowSettings, "Run").toDispatch();
 
         return Result.SUCCESS;
     }
 
     public Result stopPresentation(){
-        /*if (presentation == null && variant == null){
-            System.out.println("presentation is null");
+        try {
+            if (presentation != null) {
+                System.out.println("exiting");
+                Dispatch.call(presentation, "Save");
+                Dispatch.call(presentation, "Close");
+            }
+            return Result.SUCCESS;
+        }catch (ComFailException cfe){
+            cfe.printStackTrace();
             return Result.FAILURE;
         }
-        else{*/
-        if (powerPoint != null){
-            System.out.println(Dispatch.call(presentation, "Close"));
-            return Result.SUCCESS;
-        }
-        else return Result.FAILURE;
     }
 
 }
